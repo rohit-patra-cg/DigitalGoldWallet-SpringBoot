8e60fd9022bc040915e650fd818ecfb24d7dc370
Rohit Patra <rohit.patra@capgemini.com>
2024-09-04 00:50:31 +0530

    Added Cors Configurations

--- src/main/java/com/example/demo/security/WebSecurityConfig.java
+++ src/main/java/com/example/demo/security/WebSecurityConfig.java
@@ -6,6 +6,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.intercept.AuthorizationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class WebSecurityConfig {
@@ -19,9 +22,21 @@ public class WebSecurityConfig {
 	@Bean
 	SecurityFilterChain filterChain(HttpSecurity httpSec) throws Exception {
 		httpSec.csrf(AbstractHttpConfigurer::disable).cors(AbstractHttpConfigurer::disable)
-				.addFilterBefore(jwtRequestFilter, AuthorizationFilter.class)
-				.authorizeHttpRequests(auth -> auth.requestMatchers("/api/auth/login", "/api/v1/users/add").permitAll()
+				.cors(sec -> sec.configurationSource(corsConfigurationSource()))
+				.formLogin(AbstractHttpConfigurer::disable).addFilterBefore(jwtRequestFilter, AuthorizationFilter.class)
+				.authorizeHttpRequests(auth -> auth.requestMatchers("/api/auth/login", "/api/v1/users/add", "/api/v1/address/add").permitAll()
 						.anyRequest().authenticated());
 		return httpSec.build();
+	}
+
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowCredentials(true);
+		configuration.addAllowedOrigin("http://localhost:4200");
+		configuration.addAllowedHeader("*");
+		configuration.addAllowedMethod("*");
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
 	}
 }
