9d2565186b05320a7289db528ba5e0cb01b8b529
SARIDE <nikhilendra-kishore.saride@capgemini.com>
2024-09-04 12:57:53 +0530

    Added Comments For Vendor Branch

--- src/main/java/com/example/demo/service/VendorBranchServiceImpl.java
+++ src/main/java/com/example/demo/service/VendorBranchServiceImpl.java
@@ -35,18 +35,34 @@ public class VendorBranchServiceImpl implements VendorBranchService {
 		this.addressService = addressService;
 		this.vendorService = vendorService;
 	}
-
+	
+	/**
+	 *Get All Vendor Branches
+	 *@return List<VendorBranch> Collection of VendorBranch
+	 */
 	@Override
 	public List<VendorBranch> getAllVendorBranches() {
 		return vendorBranchRepository.findAll();
 	}
-
+	
+	/**
+	 *Get Vendor Branch by branch_id
+	 *@param VendorBranch
+	 *@return VendorBranch Object
+	 *@throws VendorBranchNotFoundException
+	 */
 	@Override
 	public VendorBranch getVendorBranchByBranchId(int branchId) throws VendorBranchNotFoundException {
 		return vendorBranchRepository.findById(branchId)
 				.orElseThrow(() -> new VendorBranchNotFoundException("Vendor Branch not found with id: " + branchId));
 	}
-
+	
+	/**
+	 * Get Vendor Branch by vendor_id
+	 * @param vendorId 
+	 * @return List<VendorBranch> Collection of VendorBranch 
+	 * @throws VendorBranchNotFoundException
+	 */
 	@Override
 	public List<VendorBranch> getVendorBranchByVendorId(int vendorId) throws VendorBranchNotFoundException {
 		List<VendorBranch> branches = vendorBranchRepository.findByBranchId(vendorId);
@@ -55,22 +71,43 @@ public class VendorBranchServiceImpl implements VendorBranchService {
 		}
 		return branches;
 	}
-
+	
+	/**
+	 *Get Vendor Branch by city
+	 *@param city
+	 *@return List<VendorBranch> Collection of VendorBranch
+	 */
 	@Override
 	public List<VendorBranch> getVendorBranchByCity(String city) {
 		return vendorBranchRepository.findByCity(city);
 	}
-
+	
+	/**
+	 *Get Vendor Branch by state
+	 *@param state
+	 *@return List<VendorBranch> Collection of VendorBranch
+	 */
 	@Override
 	public List<VendorBranch> getVendorBranchByState(String state) {
 		return vendorBranchRepository.findByState(state);
 	}
-
+	
+	/**
+	 *Get Vendor Branch by country
+	 *@param country
+	 *@return List<VendorBranch> Collection of VendorBranch
+	 */
 	@Override
 	public List<VendorBranch> getVendorBranchByCountry(String country) {
 		return vendorBranchRepository.findByCountry(country);
 	}
-
+	
+	/**
+	 * Get Vendor Branch transactions by branch_id
+	 * @param branchId
+	 * @return List<TransactionHistory> Collection of transactionHistory
+	 * @throws VendorBranchNotFoundException
+	 */
 	@Override
 	public List<TransactionHistory> getVendorBranchTransactionsByBranchId(int branchId)
 			throws VendorBranchNotFoundException {
@@ -78,6 +115,12 @@ public class VendorBranchServiceImpl implements VendorBranchService {
 		return transactionHistoryRepository.findAllByBranchId(branchId);
 	}
 	
+	/**
+	 * Add New Vendor Branch
+	 * @param branchDTO
+	 * @return SuccessResponse Response for successfully adding a new vendor branch
+	 * @throws VendorBranchNotFoundException, AddressNotFoundException
+	 */
 	@Override
     public SuccessResponse addVendorBranch(VendorBranchDTO branchDTO) throws VendorNotFoundException, AddressNotFoundException {
 		VendorBranch vendorBranch = new VendorBranch();
@@ -87,7 +130,13 @@ public class VendorBranchServiceImpl implements VendorBranchService {
 		VendorBranch savedBranch = vendorBranchRepository.save(vendorBranch);
 		return new SuccessResponse(new Date(), "Vendor Branch added successfully", savedBranch.getBranchId());
     }
-
+	
+	/**
+	 * Transfers a specified quantity of gold from one vendor branch to another
+	 * @param sourceBranchId, destinationBranchId, quantity
+	 * @return SuccessResponse Response for successfully transferring gold between branches
+	 * @throws VendorBranchNotFoundException, InvalidGoldQuantityException
+	 */
     @Override
     public SuccessResponse transferGoldBetweenBranches(int sourceBranchId, int destinationBranchId, double quantity) throws VendorBranchNotFoundException, InvalidGoldQuantityException {
         VendorBranch sourceBranch = getVendorBranchByBranchId(sourceBranchId);
@@ -101,7 +150,13 @@ public class VendorBranchServiceImpl implements VendorBranchService {
         vendorBranchRepository.save(destinationBranch);
         return new SuccessResponse(new Date(),"Vendor Branch transfer was successful", sourceBranchId);
     }
-
+    
+    /**
+	 * Update Vendorbranch details by branch_id
+	 * @param branchId, vendorBranchDTO
+	 * @return SuccessResponse Response for successfully updating Vendor Branch
+	 * @throws VendorBranchNotFoundException, AddressNotFoundException, VendorNotFoundException
+	 */
     @Override
     public SuccessResponse updateVendorBranch(int branchId, VendorBranchDTO vendorBranchDTO) throws VendorBranchNotFoundException, AddressNotFoundException, VendorNotFoundException {
         VendorBranch vendorBranch = getVendorBranchByBranchId(branchId);
